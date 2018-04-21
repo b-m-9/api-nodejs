89590de5a498590791d857eaf373797456710124
medve-dev <200.medvedev@gmail.com>
2018-04-21 11:30:54 +0300

    api

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const expressRouter = require('./modules/api/expressRouter');
+
 class API {
     constructor(config) {
-
+        this.expressRouter = expressRouter;
     }
+
 }
 
 module.exports = API;
