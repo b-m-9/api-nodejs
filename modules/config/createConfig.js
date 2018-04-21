a86caa4469b91478a1256221124c425d797de0b1
medve-dev <200.medvedev@gmail.com>
2018-04-21 12:19:43 +0300

    api

--- modules/config/createConfig.js
+++ modules/config/createConfig.js
@@ -3,14 +3,14 @@ var config = require('./index');
 var random = require('../../modules/random');
 
 // var portfinder = require('portfinder');
-config.set('project_name', 'example', true, true);
-config.set('shema', 'http', true, true);
-config.set('domain', 'localhost:3001', true, true);
-config.set('server_path', '/', true, true);
-// config.set('server_path', '/service/', true, true); // nginx conf
-config.set('api_path', 'v1/api/', true, true);
-config.set('server:session:secret', random.str(12,15), true, true);
-config.set('server:session:ttl_hours', 48, true, true);
-config.set('server:session:name', random.str(6,8), true, true);
-config.set('server:api:timeout',10, true, true);
+config.default('project_name', 'example');
+config.default('shema', 'http');
+config.default('domain', 'localhost:3001');
+config.default('server_path', '/');
+// config.default('server_path', '/service/'); // nginx conf
+config.default('api_path', 'api/v1/');
+config.default('server:session:secret', random.str(12,15));
+config.default('server:session:ttl_hours', 48);
+config.default('server:session:name', random.str(6,8));
+config.default('server:api:timeout',10);
 config.save();
