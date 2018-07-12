772707136601188812dceaac55f2a95b1da505dd
bogdanmedvedev <bogdan@medve-dev.net>
2018-07-12 16:42:41 +0300

    api limit 200mb

--- modules/api/expressRouter.js
+++ modules/api/expressRouter.js
@@ -104,8 +104,8 @@ router.get('/export/postman', (req, res) => {
     res.download(path.normalize(__dirname + '/../../_docs/postman.postman_collection'));
 });
 
-router.use(bodyParser.urlencoded({extended: true, limit: '10mb'}));
-router.use(bodyParser.json({limit: '100mb'}));
+router.use(bodyParser.urlencoded({extended: true, limit: '200mb'}));
+router.use(bodyParser.json({limit: '200mb'}));
 router.use(fileUpload({
     abortOnLimit: true,
     limits: {fileSize: 30 * 1024  * 1024},
