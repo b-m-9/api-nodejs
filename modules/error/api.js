643dff15f734f44a4db0272dc06f6a5d793427e7
b-m-9 <bogdan@medve-dev.net>
2018-12-02 14:36:59 +0300

    fix

--- modules/error/api.js
+++ modules/error/api.js
@@ -6,14 +6,14 @@ Error.stackTraceLimit = 30;
 function create_error(message, type, params, level, errorCode, statusCode) {
     let stack_ = (new Error()).stack.replace(new RegExp('    ', 'g'), '').replace(new RegExp('at ', 'g'), '').replace(new RegExp('Promise.(.*).err', 'g'), 'Promise').split("\n");
     let message_stack = '';
-    if (stack_[2] && stack_[2].indexOf('bluebird') === -1) message_stack += stack_[2];
-    if (stack_[3] && stack_[3].indexOf('bluebird') === -1) message_stack += '\n' + stack_[3];
-    if (stack_[4] && stack_[4].indexOf('bluebird') === -1) message_stack += '\n' + stack_[4];
-    if (stack_[5] && stack_[5].indexOf('bluebird') === -1) message_stack += '\n' + stack_[5];
-    if (stack_[6] && stack_[6].indexOf('bluebird') === -1) message_stack += '\n' + stack_[6];
-    if (stack_[7] && stack_[7].indexOf('bluebird') === -1) message_stack += '\n' + stack_[7];
-    if (stack_[8] && stack_[8].indexOf('bluebird') === -1) message_stack += '\n' + stack_[8];
-    if (stack_[9] && stack_[9].indexOf('bluebird') === -1) message_stack += '\n' + stack_[9];
+    if (stack_[2] && stack_[2].indexOf('bluebird') === -1 && stack_[2].indexOf('modules/error/api') === -1) message_stack += stack_[2];
+    if (stack_[3] && stack_[3].indexOf('bluebird') === -1 && stack_[3].indexOf('modules/error/api') === -1) message_stack += '\n' + stack_[3];
+    if (stack_[4] && stack_[4].indexOf('bluebird') === -1 && stack_[4].indexOf('modules/error/api') === -1) message_stack += '\n' + stack_[4];
+    if (stack_[5] && stack_[5].indexOf('bluebird') === -1 && stack_[5].indexOf('modules/error/api') === -1) message_stack += '\n' + stack_[5];
+    if (stack_[6] && stack_[6].indexOf('bluebird') === -1 && stack_[6].indexOf('modules/error/api') === -1) message_stack += '\n' + stack_[6];
+    if (stack_[7] && stack_[7].indexOf('bluebird') === -1 && stack_[7].indexOf('modules/error/api') === -1) message_stack += '\n' + stack_[7];
+    if (stack_[8] && stack_[8].indexOf('bluebird') === -1 && stack_[8].indexOf('modules/error/api') === -1) message_stack += '\n' + stack_[8];
+    if (stack_[9] && stack_[9].indexOf('bluebird') === -1 && stack_[9].indexOf('modules/error/api') === -1) message_stack += '\n' + stack_[9];
     return {
         apiError: true,
         message,
